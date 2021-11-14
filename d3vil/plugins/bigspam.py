4f4c84814add6192887dfa73752936d706c83d31
Itz-UNKOWN-xd <93468291+Itz-UNKOWN-xd@users.noreply.github.com>
2021-11-14 13:05:14 +0530

    Update bigspam.py

--- d3vil/plugins/bigspam.py
+++ d3vil/plugins/bigspam.py
@@ -19,7 +19,7 @@ import os
 from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-from .. import D3vil, D3vil2, D3vil3, D3vil4, D3vil5, D3vil6, D3vil7, D3vil8, D3vil9, D3vil10, SUDO_USERS
+from .. import UstaD, UstaD2, UstaD3, UstaD4, UstaD5, UstaD6, UstaD7, UstaD8, UstaD9, UstaD10, SUDO_USERS
 
 SMEX_USERS = []
 for x in SUDO_USERS:
@@ -27,24 +27,24 @@ for x in SUDO_USERS:
 
 
 
-@D3vil.on(events.NewMessage(pattern="/bigspam"))
-@D3vil2.on(events.NewMessage(pattern="/bigspam"))
-@D3vil3.on(events.NewMessage(pattern="/bigspam"))
-@D3vil4.on(events.NewMessage(pattern="/bigspam"))
-@D3vil5.on(events.NewMessage(pattern="/bigspam"))
-@D3vil6.on(events.NewMessage(pattern="/bigspam"))
-@D3vil7.on(events.NewMessage(pattern="/bigspam"))
-@D3vil8.on(events.NewMessage(pattern="/bigspam"))
-@D3vil9.on(events.NewMessage(pattern="/bigspam"))
-@D3vil10.on(events.NewMessage(pattern="/bigspam"))
+@UstaD.on(events.NewMessage(pattern="/bigspam"))
+@UstaD2.on(events.NewMessage(pattern="/bigspam"))
+@UstaD3.on(events.NewMessage(pattern="/bigspam"))
+@UstaD4.on(events.NewMessage(pattern="/bigspam"))
+@UstaD5.on(events.NewMessage(pattern="/bigspam"))
+@UstaD6.on(events.NewMessage(pattern="/bigspam"))
+@UstaD7.on(events.NewMessage(pattern="/bigspam"))
+@UstaD8.on(events.NewMessage(pattern="/bigspam"))
+@UstaD9.on(events.NewMessage(pattern="/bigspam"))
+@UstaD10.on(events.NewMessage(pattern="/bigspam"))
 async def spam(e):
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
-        Devil = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        Ustad = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         smex = await e.get_reply_message()
         if len(Ustad) == 2:
-            message = str(Devil[1])
-            counter = int(Devil[0])
+            message = str(Ustad[1])
+            counter = int(Ustad[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     if e.reply_to_msg_id:
@@ -53,7 +53,7 @@ async def spam(e):
                         await e.client.send_message(e.chat_id, message)
                     await asyncio.sleep(0.1)
         elif e.reply_to_msg_id and smex.media:
-            counter = int(Devil[0])
+            counter = int(Ustad[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "document"):
                     smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
@@ -61,7 +61,7 @@ async def spam(e):
                 await asyncio.sleep(0.1)
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
-            counter = int(Devil[0])
+            counter = int(Ustad[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     await e.client.send_message(e.chat_id, message)
